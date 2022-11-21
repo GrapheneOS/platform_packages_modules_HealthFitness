30da36b0d5ad7de69e8862a7699cfb81153c0bb2
Teo Georgescu <teog@google.com>
2022-11-21 14:08:20 +0000

    Added theming for text styles and applied them on the Onboarding screen.
    Test: atest
    Bug: 245515058
    Change-Id: Ib73453a152eb570106681cc34e93ae90bb849a3f

--- apk/src/com/android/healthconnect/controller/onboarding/OnboardingActivity.kt
+++ apk/src/com/android/healthconnect/controller/onboarding/OnboardingActivity.kt
@@ -3,20 +3,19 @@ package com.android.healthconnect.controller.onboarding
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import com.android.healthconnect.controller.R
 import dagger.hilt.android.AndroidEntryPoint
 
 /** Onboarding activity for Health Connect. */
-@AndroidEntryPoint(AppCompatActivity::class)
+@AndroidEntryPoint(FragmentActivity::class)
 class OnboardingActivity : Hilt_OnboardingActivity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.onboarding_screen)
-        getSupportActionBar()?.hide()
-        var goBackButton = findViewById<Button>(R.id.go_back_button)
-        var getStartedButton = findViewById<Button>(R.id.get_started_button)
+        val goBackButton = findViewById<Button>(R.id.go_back_button)
+        val getStartedButton = findViewById<Button>(R.id.get_started_button)
         goBackButton.setOnClickListener {
             setResult(Activity.RESULT_CANCELED)
             finish()
