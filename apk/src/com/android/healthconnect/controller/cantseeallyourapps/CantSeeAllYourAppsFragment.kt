b108ffdb02ac4a362cd47d2bce28899736078efa
Abel Hubena <abeltsegaye@google.com>
2022-11-23 18:41:07 +0000

    Adding implementation of functionalities of the following buttons on "Can't see all your apps?" screen:     - Check for updates     - See all compatible apps     - Send feedback
    Bug: 245515047
    Screen recording: https://drive.google.com/file/d/1PSePOTrbdRvkVJeJKszq1_kUdMvzkn0l/view?usp=sharing
    Test: atest HealthConnectControllerUITests
    Change-Id: I2035d00b92cf3d319d1c48765616370aaa1cf124

--- apk/src/com/android/healthconnect/controller/cantseeallyourapps/CantSeeAllYourAppsFragment.kt
+++ apk/src/com/android/healthconnect/controller/cantseeallyourapps/CantSeeAllYourAppsFragment.kt
@@ -1,6 +1,9 @@
 package com.android.healthconnect.controller.cantseeallyourapps
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.android.healthconnect.controller.R
 import com.android.healthconnect.controller.utils.setTitle
@@ -10,6 +13,24 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint(PreferenceFragmentCompat::class)
 class CantSeeAllYourAppsFragment : Hilt_CantSeeAllYourAppsFragment() {
 
+    companion object {
+        const val CHECK_FOR_UPDATES = "check_for_updates"
+        private const val SEE_ALL_COMPATIBLE_APPS = "see_all_compatible_apps"
+        private const val SEND_FEEDBACK = "send_feedback"
+    }
+
+    private val mCheckForUpdates: Preference? by lazy {
+        preferenceScreen.findPreference(CHECK_FOR_UPDATES)
+    }
+
+    private val mSeeAllCompatibleApps: Preference? by lazy {
+        preferenceScreen.findPreference(SEE_ALL_COMPATIBLE_APPS)
+    }
+
+    private val mSendFeedback: Preference? by lazy {
+        preferenceScreen.findPreference(SEND_FEEDBACK)
+    }
+
     override fun onResume() {
         super.onResume()
         setTitle(R.string.cant_see_all_your_apps_title)
@@ -17,5 +38,21 @@ class CantSeeAllYourAppsFragment : Hilt_CantSeeAllYourAppsFragment() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.cant_see_all_your_apps_screen, rootKey)
+
+        mCheckForUpdates?.setOnPreferenceClickListener {
+            findNavController().navigate(R.id.action_connected_apps_to_updated_apps)
+            true
+        }
+
+        mSeeAllCompatibleApps?.setOnPreferenceClickListener {
+            findNavController().navigate(R.id.action_connected_apps_to_play_store)
+            true
+        }
+
+        mSendFeedback?.setOnPreferenceClickListener {
+            val intent = Intent(Intent.ACTION_BUG_REPORT)
+            getActivity()?.startActivityForResult(intent, 0)
+            true
+        }
     }
 }
