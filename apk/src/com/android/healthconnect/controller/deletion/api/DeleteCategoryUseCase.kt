dd6dbdb9ec81c1d686642f045aa0db72f58f9014
Teo Georgescu <teog@google.com>
2022-12-02 18:34:12 +0000

    Integration with deletion APIs for all deletion entry points.
    Test: atest
    Bug: b/246775481
    Bug: b/246775482
    Bug: b/246161850
    Bug: b/246775934
    Change-Id: Ic1f03a9825883fe9e071763bf06a0a6cd030a5cc

---
+++ apk/src/com/android/healthconnect/controller/deletion/api/DeleteCategoryUseCase.kt
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * ```
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * ```
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.android.healthconnect.controller.deletion.api
+
+import android.healthconnect.DeleteUsingFiltersRequest
+import android.healthconnect.HealthConnectManager
+import android.healthconnect.TimeRangeFilter
+import android.util.Log
+import androidx.core.os.asOutcomeReceiver
+import com.android.healthconnect.controller.deletion.DeletionType
+import com.android.healthconnect.controller.service.IoDispatcher
+import com.android.healthconnect.controller.shared.HealthPermissionToDatatypeMapper
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Runnable
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
+
+@Singleton
+class DeleteCategoryUseCase
+@Inject
+constructor(
+    private val healthConnectManager: HealthConnectManager,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
+) {
+
+    suspend fun invoke(
+        deleteCategory: DeletionType.DeletionTypeCategoryData,
+        timeRangeFilter: TimeRangeFilter
+    ) {
+        val deleteRequest = DeleteUsingFiltersRequest.Builder().setTimeRangeFilter(timeRangeFilter)
+
+        deleteCategory.category.healthPermissionTypes
+            .map { healthPermissionType ->
+                HealthPermissionToDatatypeMapper.getDataTypes(healthPermissionType)
+            }
+            .flatten()
+            .map { recordType -> deleteRequest.addRecordType(recordType) }
+
+        withContext(dispatcher) {
+            suspendCancellableCoroutine<Void> { continuation ->
+                healthConnectManager.deleteRecords(
+                    deleteRequest.build(), Runnable::run, continuation.asOutcomeReceiver())
+            }
+        }
+    }
+}
---
+++ apk/tests/src/com/android/healthconnect/controller/tests/deletion/api/DeleteCategoryUseCaseTest.kt
@@ -0,0 +1,92 @@
+/**
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * ```
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * ```
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.android.healthconnect.controller.tests.deletion.api
+
+import android.healthconnect.DeleteUsingFiltersRequest
+import android.healthconnect.HealthConnectManager
+import android.healthconnect.TimeRangeFilter
+import android.healthconnect.datatypes.SpeedRecord
+import android.healthconnect.datatypes.StepsCadenceRecord
+import android.healthconnect.datatypes.StepsRecord
+import android.os.OutcomeReceiver
+import com.android.healthconnect.controller.categories.HealthDataCategory
+import com.android.healthconnect.controller.deletion.DeletionType
+import com.android.healthconnect.controller.deletion.api.DeleteCategoryUseCase
+import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import java.time.Instant
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.Mockito
+import org.mockito.Mockito.any
+import org.mockito.Mockito.doAnswer
+import org.mockito.Mockito.mock
+import org.mockito.MockitoAnnotations
+import org.mockito.invocation.InvocationOnMock
+
+@HiltAndroidTest
+class DeleteCategoryUseCaseTest {
+    @get:Rule val hiltRule = HiltAndroidRule(this)
+
+    private lateinit var useCase: DeleteCategoryUseCase
+    var manager: HealthConnectManager = mock(HealthConnectManager::class.java)
+
+    @Captor lateinit var filtersCaptor: ArgumentCaptor<DeleteUsingFiltersRequest>
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        useCase = DeleteCategoryUseCase(manager, Dispatchers.Main)
+    }
+
+    @Test
+    fun invoke_deleteCategoryData_callsHealthManager() = runTest {
+        doAnswer(prepareAnswer())
+            .`when`(manager)
+            .deleteRecords(any(DeleteUsingFiltersRequest::class.java), any(), any())
+
+        val startTime = Instant.now().minusSeconds(10)
+        val endTime = Instant.now()
+
+        val deleteCategory = DeletionType.DeletionTypeCategoryData(HealthDataCategory.ACTIVITY)
+
+        useCase.invoke(deleteCategory, TimeRangeFilter.Builder(startTime, endTime).build())
+
+        Mockito.verify(manager, Mockito.times(1))
+            .deleteRecords(filtersCaptor.capture(), any(), any())
+
+        assertThat(filtersCaptor.value.timeRangeFilter.startTime).isEqualTo(startTime)
+        assertThat(filtersCaptor.value.timeRangeFilter.endTime).isEqualTo(endTime)
+        assertThat(filtersCaptor.value.dataOrigins).isEmpty()
+        // TODO update when more records available
+        assertThat(filtersCaptor.value.recordTypes)
+            .containsExactly(
+                StepsRecord::class.java, StepsCadenceRecord::class.java, SpeedRecord::class.java)
+    }
+
+    private fun prepareAnswer(): (InvocationOnMock) -> Nothing? {
+        val answer = { args: InvocationOnMock ->
+            val receiver = args.arguments[2] as OutcomeReceiver<Void, *>
+            receiver.onResult(null)
+            null
+        }
+        return answer
+    }
+}
