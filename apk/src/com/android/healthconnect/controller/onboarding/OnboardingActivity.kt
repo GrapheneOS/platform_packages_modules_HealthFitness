96c6361de2ac14b01f522292b7894a9e8bfbe02e
Abel Hubena <abeltsegaye@google.com>
2023-01-06 14:10:57 +0000

    Fix go back button functionality from Onboarding Screen
    Bug: 264525441
    Screen recording: https://drive.google.com/file/d/1HiDzqjdq6LjnhPBTx4fgo08ir2iafk8d/view?usp=sharing
    Test: atest HealthConnectControllerUITests
    
    Change-Id: I25e1edcbef86f51c28ed69a61f91bccc8cf20c12

--- apk/src/com/android/healthconnect/controller/onboarding/OnboardingActivity.kt
+++ apk/src/com/android/healthconnect/controller/onboarding/OnboardingActivity.kt
@@ -1,9 +1,12 @@
 package com.android.healthconnect.controller.onboarding
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.fragment.app.FragmentActivity
+import com.android.healthconnect.controller.MainActivity
 import com.android.healthconnect.controller.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,13 +17,18 @@ class OnboardingActivity : Hilt_OnboardingActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.onboarding_screen)
+        val sharedPreference = getSharedPreferences("USER_ACTIVITY_TRACKER", Context.MODE_PRIVATE)
         val goBackButton = findViewById<Button>(R.id.go_back_button)
         val getStartedButton = findViewById<Button>(R.id.get_started_button)
         goBackButton.setOnClickListener {
-            setResult(Activity.RESULT_CANCELED)
+            val intent = Intent(this, MainActivity::class.java)
+            setResult(Activity.RESULT_CANCELED, intent)
             finish()
         }
         getStartedButton.setOnClickListener {
+            val editor = sharedPreference.edit()
+            editor.putBoolean(getString(R.string.previously_opened), true)
+            editor.apply()
             setResult(Activity.RESULT_OK)
             finish()
         }
