c8483866d82eede6c2b167e575ff1a012c0cba69
Abel Hubena <abeltsegaye@google.com>
2023-02-01 22:28:00 +0000

    Adding fix for back button action from feedback
    Bug: 267464537
    Screen Recording : https://drive.google.com/file/d/1mAIuVUnS9SujBr23QVsJiz-D0y0iN7da/view?usp=sharing
    Test: atest HealthConnectControllerUITests
    
    Change-Id: I5fe2d18d7b973ffef9289560846fa9f1068ab6c4

--- apk/src/com/android/healthconnect/controller/onboarding/OnboardingActivity.kt
+++ apk/src/com/android/healthconnect/controller/onboarding/OnboardingActivity.kt
@@ -1,10 +1,11 @@
 package com.android.healthconnect.controller.onboarding
 
-import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.fragment.app.FragmentActivity
+import com.android.healthconnect.controller.MainActivity
 import com.android.healthconnect.controller.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,18 +18,13 @@ class OnboardingActivity : Hilt_OnboardingActivity() {
         val sharedPreference = getSharedPreferences("USER_ACTIVITY_TRACKER", Context.MODE_PRIVATE)
         val goBackButton = findViewById<Button>(R.id.go_back_button)
         val getStartedButton = findViewById<Button>(R.id.get_started_button)
-        goBackButton.setOnClickListener {
-            /**
-             * Sets the result [Activity.RESULT_CANCELED] that will be returned to the MainActivity
-             * and finishes the activity.
-             */
-            finish()
-        }
+        goBackButton.setOnClickListener { finish() }
         getStartedButton.setOnClickListener {
             val editor = sharedPreference.edit()
             editor.putBoolean(getString(R.string.previously_opened), true)
             editor.apply()
-            setResult(Activity.RESULT_OK)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
             finish()
         }
     }
