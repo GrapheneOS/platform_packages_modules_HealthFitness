fc252b7f58782a939fd9aa43586ece6139282981
Teo Georgescu <teog@google.com>
2023-02-27 13:58:23 +0000

    Adding logging for: - Connected Apps page - Help and Feedback page - Search page - App access (connected app) page
    Refactored Connected Apps page to use dynamically generated preferences
    instead of having all (in)visible prefrences in the XML file, as this
    was causing duplicate logging.
    
    Other fixes:
    - Search apps page now uses the same preferences as connected apps
    
    Test: atest
    Test: statsd_testdrive
    
    Fix: 270767797
    Fix: 270753002
    Fix: 270772509
    Fix: 270768830
    Fix: 270847927
    Change-Id: Ib5554a2ebeaa362a4404e67dcd1c94bdc517914d

--- apk/src/com/android/healthconnect/controller/permissions/connectedapps/HealthAppPreference.kt
+++ apk/src/com/android/healthconnect/controller/permissions/connectedapps/HealthAppPreference.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.android.healthconnect.controller.permissions.connectedapps
 
 import android.content.Context
@@ -5,14 +20,42 @@ import android.text.TextUtils
 import androidx.preference.Preference
 import androidx.preference.PreferenceViewHolder
 import com.android.healthconnect.controller.shared.app.AppMetadata
+import com.android.healthconnect.controller.utils.logging.ElementName
+import com.android.healthconnect.controller.utils.logging.ErrorPageElement
+import com.android.healthconnect.controller.utils.logging.HealthConnectLogger
+import com.android.healthconnect.controller.utils.logging.HealthConnectLoggerEntryPoint
 import com.android.settingslib.widget.AppPreference
+import dagger.hilt.android.EntryPointAccessors
 
 class HealthAppPreference(context: Context, private val appMetadata: AppMetadata) :
     AppPreference(context), ComparablePreference {
 
+    private var logger: HealthConnectLogger
+    var logName : ElementName = ErrorPageElement.UNKNOWN_ELEMENT
+
     init {
         title = appMetadata.appName
         icon = appMetadata.icon
+
+        val hiltEntryPoint =
+            EntryPointAccessors.fromApplication(
+                context.applicationContext, HealthConnectLoggerEntryPoint::class.java)
+        logger = hiltEntryPoint.logger()
+    }
+
+    override fun onAttached() {
+        super.onAttached()
+        logger.logImpression(logName)
+    }
+
+    override fun setOnPreferenceClickListener(
+        onPreferenceClickListener: OnPreferenceClickListener
+    ) {
+        val loggingClickListener = OnPreferenceClickListener {
+            logger.logInteraction(logName)
+            onPreferenceClickListener.onPreferenceClick(it)
+        }
+        super.setOnPreferenceClickListener(loggingClickListener)
     }
 
     override fun isSameItem(preference: Preference): Boolean {
