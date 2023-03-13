b96e76049119b3188f5e57b774481dd10875fd8e
Aditya Choudhary <caditya@google.com>
2023-03-13 11:47:56 +0000

    Handle null endpoints in TimeRangeFilter. Bug:272760519
    Change-Id: I1e6571293e98e12e16165acbf6fa804d8a80d1d2

--- framework/java/android/health/connect/TimeRangeFilterHelper.java
+++ framework/java/android/health/connect/TimeRangeFilterHelper.java
@@ -76,7 +76,7 @@ public final class TimeRangeFilterHelper {
         } else if (timeRangeFilter instanceof LocalTimeRangeFilter) {
             return ((LocalTimeRangeFilter) timeRangeFilter)
                     .getStartTime()
-                    .toInstant(ZoneOffset.MIN)
+                    .toInstant(ZoneOffset.MAX)
                     .toEpochMilli();
         } else {
             throw new IllegalArgumentException(
@@ -94,7 +94,7 @@ public final class TimeRangeFilterHelper {
         } else if (timeRangeFilter instanceof LocalTimeRangeFilter) {
             return ((LocalTimeRangeFilter) timeRangeFilter)
                     .getEndTime()
-                    .toInstant(ZoneOffset.MAX)
+                    .toInstant(ZoneOffset.MIN)
                     .toEpochMilli();
         } else {
             throw new IllegalArgumentException(
