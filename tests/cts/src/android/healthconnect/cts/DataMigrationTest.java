a85c38e55750ff4bcc5e315554454675bf984983
Arkadii Ivanov <arkivanov@google.com>
2023-03-27 17:42:21 +0100

    Disable deduplication by entityId during migration for record.
    Fix: 275312928
    Test: atest DataMigrationTest
    Change-Id: I5434c06e9e8a29d6d6dc3b83768ee376fcc642f2

--- tests/cts/src/android/healthconnect/cts/DataMigrationTest.java
+++ tests/cts/src/android/healthconnect/cts/DataMigrationTest.java
@@ -277,20 +277,20 @@ public class DataMigrationTest {
     }
 
     @Test
-    public void migrateRecord_sameEntityId_ignored() {
+    public void migrateRecord_sameEntityId_notIgnored() {
         final String entityId = "height";
 
         final Length height1 = fromMeters(3D);
         migrate(new HeightRecord.Builder(getMetadata(), mEndTime, height1).build(), entityId);
 
         final Length height2 = fromMeters(1D);
-        migrate(new HeightRecord.Builder(getMetadata(), mEndTime, height2).build(), entityId);
+        migrate(new HeightRecord.Builder(getMetadata(), mStartTime, height2).build(), entityId);
 
         finishMigration();
 
         final List<HeightRecord> records = getRecords(HeightRecord.class);
         mExpect.that(records.stream().map(HeightRecord::getHeight).toList())
-                .containsExactly(height1);
+                .containsExactly(height1, height2);
     }
 
     @Test
