8c4234353d85b6a504e461eb542582ea3ea76e81
Magdi <magdi@google.com>
2023-05-17 14:41:10 +0000

    Fix BMR converstion from power to kcalories
    Using the same conversion as the apk uses to convert data from kcalories
    to power here https://source.corp.google.com/piper///depot/google3/java/com/google/android/apps/healthdata/migration/writer/converters/UnitConverters.kt;rcl=532291834;l=9
    
    Fix: 281679766
    Test: atest HealthConnectControllerUITests
    Change-Id: Icc2ce9bf18dcc588ac69a4d114c70f90255b0f78

--- apk/src/com/android/healthconnect/controller/dataentries/units/PowerConverter.kt
+++ apk/src/com/android/healthconnect/controller/dataentries/units/PowerConverter.kt
@@ -3,9 +3,11 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
+ *
  * ```
  *      http://www.apache.org/licenses/LICENSE-2.0
  * ```
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -17,7 +19,7 @@ import kotlin.math.roundToLong
 
 /** Power conversion utilities. */
 object PowerConverter {
-    private const val CALORIES_PER_WATT = 859.84522785899
+    private const val CALORIES_PER_WATT = 20.65
 
     /**
      * Converts from watt to calories/day
