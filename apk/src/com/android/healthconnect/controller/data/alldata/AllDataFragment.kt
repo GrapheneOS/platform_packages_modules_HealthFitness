a39eb916a0316a04c625ba917d41e0323d19bf7b
csitari <csitari@google.com>
2023-09-20 22:03:11 +0000

    New AllData screen that combines the old Categories and DataTypes screens
    Figma: https://www.figma.com/file/7qqoX5GoACLiaCrW6JaC0n/Health-Connect-UI?type=design&node-id=17883-117280&mode=design&t=itTwuodJxTUNVZyT-0
    
    Also added "No data" empty states for both AllData and AppData screens. https://www.figma.com/file/7qqoX5GoACLiaCrW6JaC0n/Health-Connect-UI?type=design&node-id=19720-126816&mode=design&t=itTwuodJxTUNVZyT-0
    
    Bug: 301318320
    Bug: 291249677
    Test: atest
    Change-Id: I682d9d26971a3ae0d62ad5e0290ae206621a9b8e

--- apk/src/com/android/healthconnect/controller/data/alldata/AllDataFragment.kt
+++ apk/src/com/android/healthconnect/controller/data/alldata/AllDataFragment.kt
@@ -13,85 +13,56 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.android.healthconnect.controller.data.appdata
+package com.android.healthconnect.controller.data.alldata
 
-import android.content.Intent.EXTRA_PACKAGE_NAME
 import android.os.Bundle
 import android.view.View
-import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceCategory
 import com.android.healthconnect.controller.R
+import com.android.healthconnect.controller.data.appdata.PermissionTypesPerCategory
 import com.android.healthconnect.controller.permissions.data.HealthPermissionStrings
-import com.android.healthconnect.controller.permissions.shared.Constants
 import com.android.healthconnect.controller.shared.HealthDataCategoryExtensions.icon
 import com.android.healthconnect.controller.shared.HealthDataCategoryExtensions.uppercaseTitle
 import com.android.healthconnect.controller.shared.preference.HealthPreference
 import com.android.healthconnect.controller.shared.preference.HealthPreferenceFragment
 import com.android.healthconnect.controller.shared.preference.NoDataPreference
-import com.android.settingslib.widget.AppHeaderPreference
+import com.android.healthconnect.controller.utils.logging.HealthConnectLogger
 import com.android.settingslib.widget.FooterPreference
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-/** Fragment to display data in Health Connect written by a given app. */
+/** Fragment for health permission types. */
 @AndroidEntryPoint(HealthPreferenceFragment::class)
-open class AppDataFragment : Hilt_AppDataFragment() {
+open class AllDataFragment : Hilt_AllDataFragment() {
 
     companion object {
-        private const val TAG = "AppDataFragmentTag"
-        const val PERMISSION_TYPE_KEY = "permission_type_key"
+        private const val TAG = "AllDataFragmentTag"
     }
 
-    init {
-        // TODO(b/281811925):
-        // this.setPageName(PageName.APP_DATA_PAGE)
-    }
+    @Inject lateinit var logger: HealthConnectLogger
 
-    private var packageName: String = ""
-    private var appName: String = ""
-
-    private val viewModel: AppDataViewModel by viewModels()
-
-    private lateinit var header: AppHeaderPreference
+    private val viewModel: AllDataViewModel by viewModels()
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         super.onCreatePreferences(savedInstanceState, rootKey)
         setPreferencesFromResource(R.xml.app_data_screen, rootKey)
-
-        if (requireArguments().containsKey(EXTRA_PACKAGE_NAME) &&
-            requireArguments().getString(EXTRA_PACKAGE_NAME) != null) {
-            packageName = requireArguments().getString(EXTRA_PACKAGE_NAME)!!
-        }
-        if (requireArguments().containsKey(Constants.EXTRA_APP_NAME) &&
-            requireArguments().getString(Constants.EXTRA_APP_NAME) != null) {
-            appName = requireArguments().getString(Constants.EXTRA_APP_NAME)!!
-        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.loadAppInfo(packageName)
-        viewModel.loadAppData(packageName)
+        viewModel.loadAllData()
 
-        header = AppHeaderPreference(requireContext())
-        viewModel.appInfo.observe(viewLifecycleOwner) { appMetadata ->
-            header.apply {
-                icon = appMetadata.icon
-                title = appMetadata.appName
-            }
-        }
-
-        viewModel.appData.observe(viewLifecycleOwner) { state ->
+        viewModel.allData.observe(viewLifecycleOwner) { state ->
             when (state) {
-                is AppDataViewModel.AppDataState.Loading -> {
+                is AllDataViewModel.AllDataState.Loading -> {
                     setLoading(isLoading = true)
                 }
-                is AppDataViewModel.AppDataState.Error -> {
+                is AllDataViewModel.AllDataState.Error -> {
                     setError(hasError = true)
                 }
-                is AppDataViewModel.AppDataState.WithData -> {
+                is AllDataViewModel.AllDataState.WithData -> {
                     setLoading(isLoading = false)
                     setError(hasError = false)
                     updatePreferenceScreen(state.dataMap)
@@ -104,7 +75,6 @@ open class AppDataFragment : Hilt_AppDataFragment() {
         permissionTypesPerCategoryList: List<PermissionTypesPerCategory>
     ) {
         preferenceScreen?.removeAll()
-        preferenceScreen.addPreference(header)
 
         val populatedCategories =
             permissionTypesPerCategoryList
@@ -137,18 +107,19 @@ open class AppDataFragment : Hilt_AppDataFragment() {
                             it.setTitle(
                                 HealthPermissionStrings.fromPermissionType(permissionType)
                                     .uppercaseLabel)
-                            it.setOnPreferenceClickListener {
-                                // TODO(b/281811925): Add in upcoming cl.
-                                // it.logName = AppDataElement.PERMISSION_TYPE_BUTTON
-                                findNavController()
-                                    .navigate(
-                                        R.id.action_appData_to_appEntries,
-                                        bundleOf(
-                                            EXTRA_PACKAGE_NAME to packageName,
-                                            Constants.EXTRA_APP_NAME to appName,
-                                            PERMISSION_TYPE_KEY to permissionType))
-                                true
-                            }
+                            // TODO(b/291249677): Add in upcoming CL.
+                            // it.logName = AllDataElement.PERMISSION_TYPE_BUTTON
+                            //                            it.setOnPreferenceClickListener {
+                            //                                findNavController()
+                            //                                    .navigate(
+                            //
+                            // R.id.action_allData_to_entriesAndAccess,
+                            //                                        bundleOf(
+                            //
+                            // HealthPermissionTypesFragment.PERMISSION_TYPE_KEY to permissionType
+                            //                                        ))
+                            //                                true
+                            //                            }
                         })
                 }
         }
