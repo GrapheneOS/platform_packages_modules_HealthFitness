72e2c262cf4c40b690b9041cf2f602d33e5b5001
Nick Fortescue <nfortescue@google.com>
2025-04-28 09:03:51 +0000

    Enable some unit tests as robolectric tests.
    For now, these set a system property. In future the infrastructure should be changed to enable the tests to be truly bivalent - no special change needed for bolectric.
    See the related bug for more info.
    
    Bug: b/403334845
    Test: atest HealthFitnessUnitTests
    Flag: TEST_ONLY
    Change-Id: Ib4b59340cc9197e5c0467fe964bb1416c11bb5d3

--- tests/unittests/src/com/android/server/healthconnect/backuprestore/CloudRestoreManagerTest.java
+++ tests/unittests/src/com/android/server/healthconnect/backuprestore/CloudRestoreManagerTest.java
@@ -73,6 +73,7 @@ import com.android.server.healthconnect.testing.storage.TransactionTestUtils;
 import com.google.common.collect.ImmutableMap;
 
 import org.jetbrains.annotations.NotNull;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -118,6 +119,26 @@ public class CloudRestoreManagerTest {
     @Mock private FirstGrantTimeManager mFirstGrantTimeManager;
     // TODO(b/373322447): Remove the mock HealthPermissionIntentAppsTracker
     @Mock private HealthPermissionIntentAppsTracker mPermissionIntentAppsTracker;
+
+    private String mPreviousRobolectricProperty;
+
+    @Before
+    public void robolectricProperties() {
+        // TODO: b/403334845 - remove this when test infrastructure supports setting this property
+        mPreviousRobolectricProperty =
+                System.setProperty("robolectric.useValidGetApplicationIcon", "true");
+    }
+
+    @After
+    public void robolectricPropertiesClear() {
+        // TODO: b/403334845 - remove this when test infrastructure supports setting this property
+        if (mPreviousRobolectricProperty == null) {
+            System.clearProperty("robolectric.useValidGetApplicationIcon");
+        } else {
+            System.setProperty(
+                    "robolectric.useValidGetApplicationIcon", mPreviousRobolectricProperty);
+        }
+    }
 
     @Before
     public void setUp() {
