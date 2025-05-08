b907b45c1a31d82e9108781e3d9460264a9ddc65
Patrick Dumdum <patricked@google.com>
2025-05-08 15:37:09 +0000

    Added data type formatters (A - BL).
    Bug: 407981868
    Flag: NONE testapp
    Test: atest
    Change-Id: Ica8412ffe2b3ad5e37bd5b4d0747ebb4d063e68c

---
+++ testapps/toolbox/tests/src/com/android/healthconnect/testapps/toolbox/read/dataentries/formatters/BasalBodyTemperatureFormatterTest.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.healthconnect.testapps.toolbox.read.dataentries.formatters
+
+import android.content.Context
+import android.health.connect.datatypes.BasalBodyTemperatureRecord
+import android.health.connect.datatypes.BodyTemperatureMeasurementLocation.MEASUREMENT_LOCATION_TOE
+import android.health.connect.datatypes.BodyTemperatureMeasurementLocation.MEASUREMENT_LOCATION_UNKNOWN
+import android.health.connect.datatypes.units.Temperature
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.healthconnect.testapps.toolbox.utils.GeneralUtils.Companion.getMetaData
+import com.google.common.truth.Truth.assertThat
+import java.time.Instant
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BasalBodyTemperatureFormatterTest {
+    private val formatter = BasalBodyTemperatureFormatter()
+    private val context: Context = ApplicationProvider.getApplicationContext()
+
+    @Test
+    fun formatBasalBodyTemperatureValue_returnsFormattedEntry_noMeasurementLocation() {
+        val record = buildBasalBodyTemperatureRecord(MEASUREMENT_LOCATION_UNKNOWN, 33.0).build()
+
+        val formattedEntry = formatter.format(record, context)
+
+        assertThat(formattedEntry.value).isEqualTo("33.0 celsius ")
+    }
+
+    @Test
+    fun formatBasalBodyTemperatureValue_returnsFormattedEntry_measurementLocation() {
+        val record = buildBasalBodyTemperatureRecord(MEASUREMENT_LOCATION_TOE, 34.1).build()
+
+        val formattedEntry = formatter.format(record, context)
+
+        assertThat(formattedEntry.value).isEqualTo("34.1 celsius Toe")
+    }
+
+    private fun buildBasalBodyTemperatureRecord(
+        measurementLocation: Int,
+        temperature: Double,
+    ): BasalBodyTemperatureRecord.Builder {
+        val NOW: Instant = Instant.parse("2024-04-10T15:36:18.000Z")
+        return BasalBodyTemperatureRecord.Builder(
+            getMetaData(context),
+            NOW,
+            measurementLocation,
+            Temperature.fromCelsius(temperature),
+        )
+    }
+}
