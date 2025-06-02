526cfb7833bb26e5df7fb283c37ea14a9fed88a4
James Stembridge <jstembridge@google.com>
2025-06-02 11:05:44 +0100

    Add workarounds for test failures on 25Q3
    25Q3 has M-2025-05 prebuilt, which doesn't include the fixes in
    ag/31040458 and ag/33310669.
    
    Bug: 421834796
    Bug: 419656395
    Bug: 415891915
    Bug: 414451502
    Flag: TEST_ONLY
    Test: atest on 25Q3-release emulator
    Change-Id: If4cd3d28f6e9aaced177d9526c187ab6fd919451

--- tests/cts/multiapptests/src/android/healthconnect/cts/device/HealthConnectDeviceTest.java
+++ tests/cts/multiapptests/src/android/healthconnect/cts/device/HealthConnectDeviceTest.java
@@ -24,6 +24,7 @@ import static android.healthconnect.testing.cts.PermissionUtils.grantAllHealthPe
 import static android.healthconnect.testing.cts.PermissionUtils.grantHealthPermission;
 import static android.healthconnect.testing.cts.PermissionUtils.revokeAllHealthPermissions;
 import static android.healthconnect.testing.cts.PermissionUtils.revokeHealthPermission;
+import static android.healthconnect.testing.cts.TestUtils.connectAppsWithGrantedPermissions;
 import static android.healthconnect.testing.cts.TestUtils.createReadRecordsRequestUsingFilters;
 import static android.healthconnect.testing.cts.TestUtils.deleteAllFitnessDataAddedByTestApp;
 import static android.healthconnect.testing.cts.TestUtils.deleteAllStagedRemoteData;
@@ -173,6 +174,8 @@ public class HealthConnectDeviceTest {
         grantAllHealthPermissions(APP_A_WITH_READ_WRITE_PERMS.getPackageName());
         grantAllHealthPermissions(APP_B_WITH_READ_WRITE_PERMS.getPackageName());
         grantAllHealthPermissions(APP_WITH_WRITE_PERMS_ONLY.getPackageName());
+        // TODO(b/421834796): Remove once AppInfo is created on demand on all tested branches.
+        connectAppsWithGrantedPermissions();
     }
 
     @After
