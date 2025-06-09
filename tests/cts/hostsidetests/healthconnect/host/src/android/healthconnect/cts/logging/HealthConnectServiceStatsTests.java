b9c9d7e9b0b8f395fa612aa1a8eb41b234780f8e
Sameer Jain <sameerj@google.com>
2025-06-09 10:48:14 +0000

    Check for supported device in teardown.
    Fix: 422783257
    Flag: NONE test only
    Test: presubmit
    Change-Id: I4d7bace177eabec108755089a28db443c500fe60

--- tests/cts/hostsidetests/healthconnect/host/src/android/healthconnect/cts/logging/HealthConnectServiceStatsTests.java
+++ tests/cts/hostsidetests/healthconnect/host/src/android/healthconnect/cts/logging/HealthConnectServiceStatsTests.java
@@ -93,6 +93,9 @@ public class HealthConnectServiceStatsTests extends BaseHostJUnit4Test implement
 
     @After
     public void tearDown() throws Exception {
+        if (!isHardwareSupported(getDevice())) {
+            return;
+        }
         // TODO(b/313055175): Do not disable rate limiting once b/300238889 is resolved.
         HostSideTestUtil.restoreRateLimitingFeatureFlag(getDevice());
         ConfigUtils.removeConfig(getDevice());
