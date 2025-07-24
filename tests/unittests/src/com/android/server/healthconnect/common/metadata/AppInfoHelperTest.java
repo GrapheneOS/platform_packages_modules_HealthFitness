6b0ee5e69739d80fb2f9f2b4e3a9b8727eda3b8a
Denis McKaigue <dmckaigue@google.com>
2025-07-24 15:47:09 +0000

    Fix caching issue for DDP display name special case
    Special casing is applied to the "android" package so that instead of
    naively using the "Android System" name from PackageManager, we use the
    device display name. Whenever the in-memory cache is first populated, we
    apply this special casing. However, when the "android" package is first
    added, after the cache has been populated, we add it directly to the
    cache. In this scenario, we weren't correctly applying the special
    casing, leading to the "Android System" name showing up in various
    places. This issue would disappear after a reboot due to the cache
    getting repopulated.
    
    This fix ensures that we apply the special casing when we add it
    directly to the map, after initial population.
    
    Fix: 431125379
    Test: AppInfoHelperTest
    Flag: com.android.healthfitness.flags.step_tracking_enabled
    Change-Id: I06e6072f1bfce783498f33d0653324b364af3e18

--- tests/unittests/src/com/android/server/healthconnect/common/metadata/AppInfoHelperTest.java
+++ tests/unittests/src/com/android/server/healthconnect/common/metadata/AppInfoHelperTest.java
@@ -136,7 +136,7 @@ public class AppInfoHelperTest {
 
     @Test
     @EnableFlags(Flags.FLAG_STEP_TRACKING_ENABLED)
-    public void getAppInfoMap_deviceProviderNameUpdated_whenFlagEnabled()
+    public void deviceDataProvider_addedToAppInfo_cacheRepopulated_deviceDisplayNameUsed()
             throws PackageManager.NameNotFoundException {
         setAppAsNotInstalled(DEVICE_PROVIDER_PACKAGE_NAME);
         setAppAsNotInstalled(TEST_PACKAGE_NAME);
@@ -164,6 +164,46 @@ public class AppInfoHelperTest {
                                 200)));
 
         mAppInfoHelper.clearCache();
+        Map<String, AppInfoInternal> appInfoInternalMap = mAppInfoHelper.getAppInfoMap();
+
+        assertThat(appInfoInternalMap).containsKey(DEVICE_PROVIDER_PACKAGE_NAME);
+        AppInfo deviceAppInfo = appInfoInternalMap.get(DEVICE_PROVIDER_PACKAGE_NAME).toExternal();
+        assertThat(deviceAppInfo.getName()).isEqualTo(EXPECTED_DEVICE_APP_NAME);
+
+        assertThat(appInfoInternalMap).containsKey(TEST_PACKAGE_NAME);
+        AppInfo testAppInfo = appInfoInternalMap.get(TEST_PACKAGE_NAME).toExternal();
+        assertThat(testAppInfo.getName()).isEqualTo(TEST_APP_NAME);
+    }
+
+    @Test
+    @EnableFlags(Flags.FLAG_STEP_TRACKING_ENABLED)
+    public void deviceDataProvider_addedToAppInfo_cacheAlreadyPopulated_deviceDisplayNameUsed()
+            throws PackageManager.NameNotFoundException {
+        setAppAsNotInstalled(DEVICE_PROVIDER_PACKAGE_NAME);
+        setAppAsNotInstalled(TEST_PACKAGE_NAME);
+        mFitnessTestUtils.insertApp(DEVICE_PROVIDER_PACKAGE_NAME);
+        mAppInfoHelper.updateAppInfoIfNotInstalled(
+                DEVICE_PROVIDER_PACKAGE_NAME, ORIGINAL_DEVICE_APP_NAME, null);
+        mFitnessTestUtils.insertApp(TEST_PACKAGE_NAME);
+        mAppInfoHelper.updateAppInfoIfNotInstalled(TEST_PACKAGE_NAME, TEST_APP_NAME, null);
+        Instant now = Instant.now();
+        mFitnessTestUtils.insertRecords(
+                DEVICE_PROVIDER_PACKAGE_NAME,
+                List.of(
+                        RecordInternalFactory.buildStepsRecord(
+                                UUID.randomUUID().toString(),
+                                now.toEpochMilli(),
+                                now.plusSeconds(1).toEpochMilli(),
+                                100)));
+        mFitnessTestUtils.insertRecords(
+                TEST_PACKAGE_NAME,
+                List.of(
+                        RecordInternalFactory.buildStepsRecord(
+                                UUID.randomUUID().toString(),
+                                now.plusSeconds(10).toEpochMilli(),
+                                now.plusSeconds(11).toEpochMilli(),
+                                200)));
+
         Map<String, AppInfoInternal> appInfoInternalMap = mAppInfoHelper.getAppInfoMap();
 
         assertThat(appInfoInternalMap).containsKey(DEVICE_PROVIDER_PACKAGE_NAME);
