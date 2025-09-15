ae0c12f3622a16e0767c9767f0bf699df02104c0
Bhupinder Dhoofer <bhupindersd@google.com>
2025-09-15 14:34:35 +0000

    Add a method to write DDP data into the Device Data Provider table.
    For a combination of `sourcePackageName`, `deviceInfoId` and `dataType`, we'll store the states `is_available` and `is_user_enabled`. Whenever the state changes, we'll update the respective row.
    
    Bug: 437877430
    Flag: com.android.healthfitness.flags.development_database
    Test: unit tests
    Change-Id: I53e022b400374d607b7b03b91dc78bf016c68d51

--- service/java/com/android/server/healthconnect/fitness/helpers/DeviceDataProviderHelper.java
+++ service/java/com/android/server/healthconnect/fitness/helpers/DeviceDataProviderHelper.java
@@ -16,19 +16,33 @@
 package com.android.server.healthconnect.fitness.helpers;
 
 import static com.android.server.healthconnect.fitness.recordhelpers.RecordHelper.DEVICE_INFO_ID_COLUMN_NAME;
+import static com.android.server.healthconnect.storage.request.UpsertTableRequest.TYPE_STRING;
 import static com.android.server.healthconnect.storage.utils.StorageUtils.INTEGER_NOT_NULL;
 import static com.android.server.healthconnect.storage.utils.StorageUtils.PRIMARY;
 import static com.android.server.healthconnect.storage.utils.StorageUtils.TEXT_NOT_NULL;
+import static com.android.server.healthconnect.storage.utils.StorageUtils.getCursorInt;
+import static com.android.server.healthconnect.storage.utils.StorageUtils.getCursorString;
+import static com.android.server.healthconnect.storage.utils.StorageUtils.getIntegerAndConvertToBoolean;
 
+import android.annotation.Nullable;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.health.connect.internal.datatypes.utils.HealthConnectMappings;
 import android.util.Pair;
 
 import com.android.server.healthconnect.common.metadata.DeviceInfoHelper;
+import com.android.server.healthconnect.device.DeviceDataSourceAdvertisement;
+import com.android.server.healthconnect.device.DeviceDataSourceState;
 import com.android.server.healthconnect.fitness.recordhelpers.RecordHelper;
 import com.android.server.healthconnect.storage.DatabaseHelper;
+import com.android.server.healthconnect.storage.TransactionManager;
 import com.android.server.healthconnect.storage.request.CreateTableRequest;
+import com.android.server.healthconnect.storage.request.ReadTableRequest;
+import com.android.server.healthconnect.storage.request.UpsertTableRequest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * A class to help with the DB transaction for storing Device Data Provider metadata and state.
@@ -43,8 +57,38 @@ public class DeviceDataProviderHelper extends DatabaseHelper {
     public static final String IS_AVAILABLE = "is_available";
     public static final String IS_USER_ENABLED = "is_user_enabled";
 
-    public DeviceDataProviderHelper(DatabaseHelpers databaseHelpers) {
+    // Only update the states isAvailable and isUserEnabled for each sourcePackageName, deviceInfoId
+    // and dataType "key"
+    public static final List<Pair<String, Integer>> UNIQUE_COLUMN_INFO =
+            List.of(
+                    new Pair<>(SOURCE_PACKAGE_NAME, TYPE_STRING),
+                    new Pair<>(DEVICE_INFO_ID_COLUMN_NAME, TYPE_STRING),
+                    new Pair<>(DATA_TYPE, TYPE_STRING));
+
+    private final TransactionManager mTransactionManager;
+    private final HealthConnectMappings mHealthConnectMappings;
+
+    private record DeviceDataProviderKey(
+            String sourcePackageName, int deviceInfoId, int dataType) {}
+
+    private record DeviceDataProviderInfo(
+            DeviceDataProviderKey key, boolean isAvailable, boolean isUserEnabled) {}
+
+    @Nullable
+    private volatile ConcurrentHashMap<DeviceDataProviderKey, DeviceDataProviderInfo> mDdpCache;
+
+    public DeviceDataProviderHelper(
+            DatabaseHelpers databaseHelpers,
+            TransactionManager transactionManager,
+            HealthConnectMappings healthConnectMappings) {
         super(databaseHelpers);
+        this.mTransactionManager = transactionManager;
+        this.mHealthConnectMappings = healthConnectMappings;
+    }
+
+    @Override
+    public synchronized void clearCache() {
+        mDdpCache = null;
     }
 
     /**
@@ -53,10 +97,84 @@ public class DeviceDataProviderHelper extends DatabaseHelper {
      */
     public static CreateTableRequest getCreateTableRequest() {
         return new CreateTableRequest(TABLE_NAME, getColumnInfo())
+                .addUniqueConstraints(
+                        List.of(SOURCE_PACKAGE_NAME, DEVICE_INFO_ID_COLUMN_NAME, DATA_TYPE))
                 .addForeignKey(
                         /* referencedTable= */ DeviceInfoHelper.TABLE_NAME,
                         /* columnNames= */ List.of(DEVICE_INFO_ID_COLUMN_NAME),
                         /* referencedColumnNames= */ List.of(RecordHelper.PRIMARY_COLUMN_NAME));
+    }
+
+    /** Updates the database with the provided Device Data Provider information. */
+    public synchronized void insertOrUpdateAdvertisement(
+            String sourcePackageName,
+            int deviceInfoId,
+            DeviceDataSourceAdvertisement deviceDataSourceAdvertisement) {
+        for (DeviceDataSourceState state :
+                deviceDataSourceAdvertisement.getDeviceDataSourceState()) {
+            int dataType = mHealthConnectMappings.getRecordType(state.getDataType());
+            DeviceDataProviderKey key =
+                    new DeviceDataProviderKey(sourcePackageName, deviceInfoId, dataType);
+            DeviceDataProviderInfo ddpInfo =
+                    new DeviceDataProviderInfo(key, state.isAvailable(), state.isUserEnabled());
+
+            if (!getDdpMap().containsKey(key) || !getDdpMap().get(key).equals(ddpInfo)) {
+                insertOrUpdate(ddpInfo);
+            }
+        }
+    }
+
+    /**
+     * Inserts ddpInfo if not present in the db or updates the states isAvailable and isUserEnabled
+     * for the given sourcePackageName, deviceInfoId and dataType.
+     */
+    private synchronized void insertOrUpdate(DeviceDataProviderInfo ddpInfo) {
+        getDdpMap().remove(ddpInfo.key);
+        mTransactionManager.insertOrReplaceOnConflict(
+                new UpsertTableRequest(TABLE_NAME, getContentValues(ddpInfo), UNIQUE_COLUMN_INFO));
+        getDdpMap().put(ddpInfo.key, ddpInfo);
+    }
+
+    private ConcurrentHashMap<DeviceDataProviderKey, DeviceDataProviderInfo> getDdpMap() {
+        if (mDdpCache == null) {
+            mDdpCache = generateDdpCache(mTransactionManager);
+        }
+        return mDdpCache;
+    }
+
+    private static synchronized ConcurrentHashMap<DeviceDataProviderKey, DeviceDataProviderInfo>
+            generateDdpCache(TransactionManager transactionManager) {
+        ConcurrentHashMap<DeviceDataProviderKey, DeviceDataProviderInfo> ddpInfoMap =
+                new ConcurrentHashMap<>();
+        try (Cursor cursor = transactionManager.read(new ReadTableRequest(TABLE_NAME))) {
+            while (cursor.moveToNext()) {
+                int deviceInfoId = getCursorInt(cursor, DEVICE_INFO_ID_COLUMN_NAME);
+                String sourcePackageName = getCursorString(cursor, SOURCE_PACKAGE_NAME);
+                int dataType = getCursorInt(cursor, DATA_TYPE);
+                boolean isAvailable = getIntegerAndConvertToBoolean(cursor, IS_AVAILABLE);
+                boolean isUserEnabled = getIntegerAndConvertToBoolean(cursor, IS_USER_ENABLED);
+
+                DeviceDataProviderKey key =
+                        new DeviceDataProviderKey(sourcePackageName, deviceInfoId, dataType);
+                DeviceDataProviderInfo ddpInfo =
+                        new DeviceDataProviderInfo(key, isAvailable, isUserEnabled);
+                ddpInfoMap.put(key, ddpInfo);
+            }
+        }
+
+        return ddpInfoMap;
+    }
+
+    private ContentValues getContentValues(DeviceDataProviderInfo ddpInfo) {
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(DEVICE_INFO_ID_COLUMN_NAME, ddpInfo.key.deviceInfoId);
+        contentValues.put(SOURCE_PACKAGE_NAME, ddpInfo.key.sourcePackageName);
+        contentValues.put(DATA_TYPE, ddpInfo.key.dataType);
+        contentValues.put(IS_AVAILABLE, ddpInfo.isAvailable);
+        contentValues.put(IS_USER_ENABLED, ddpInfo.isUserEnabled);
+
+        return contentValues;
     }
 
     /**
@@ -72,7 +190,7 @@ public class DeviceDataProviderHelper extends DatabaseHelper {
         columnInfo.add(new Pair<>(RecordHelper.PRIMARY_COLUMN_NAME, PRIMARY));
         columnInfo.add(new Pair<>(DEVICE_INFO_ID_COLUMN_NAME, INTEGER_NOT_NULL)); // Foreign key
         columnInfo.add(new Pair<>(SOURCE_PACKAGE_NAME, TEXT_NOT_NULL));
-        columnInfo.add(new Pair<>(DATA_TYPE, TEXT_NOT_NULL));
+        columnInfo.add(new Pair<>(DATA_TYPE, INTEGER_NOT_NULL));
         columnInfo.add(new Pair<>(IS_AVAILABLE, INTEGER_NOT_NULL));
         columnInfo.add(new Pair<>(IS_USER_ENABLED, INTEGER_NOT_NULL));
 
