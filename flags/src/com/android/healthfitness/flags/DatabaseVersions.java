a91d3588b6e8760e392e7e1469d28d237d376873
Nickel Cheng <nickelc@google.com>
2025-11-12 10:53:18 +0000

    Bump cycle phase db version to 25
    This is because the table name is changed. Bumping the version up so the
    updates are picked up.
    
    Not dropping the old table because the flag is only in trunk_staging.
    
    Bug: 450552432
    Test: atest
    Flag: com.android.healthfitness.flags.cycle_phases_db
    Change-Id: Ic406ece1e9ed342f55744fd17ab978dd83c8e836

--- flags/src/com/android/healthfitness/flags/DatabaseVersions.java
+++ flags/src/com/android/healthfitness/flags/DatabaseVersions.java
@@ -48,7 +48,8 @@ public final class DatabaseVersions {
     public static final int DB_VERSION_NICOTINE_INTAKE = 21;
     public static final int DB_VERSION_SYMPTOMS = 22;
     public static final int DB_VERSION_ALCOHOL_CONSUMPTION = 23;
-    public static final int DB_VERSION_MENSTRUAL_CYCLE_PHASE = 24;
+    // skip version 24, which introduced legacy table name of "cycle_phases_record_table"
+    public static final int DB_VERSION_MENSTRUAL_CYCLE_PHASE = 25;
 
     // For historical reasons, we do not support versions below this
     // See go/hc-mainline-dev/trunk_stable/db-and-aconfig#a-bit-of-history
