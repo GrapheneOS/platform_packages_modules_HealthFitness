5b430f51e216bf6ee518468c79efdb71f4f3923a
Nickel Cheng <nickelc@google.com>
2026-01-12 10:39:42 -0800

    Revert^2 "Add permission enforcement for Device UDI"
    This reverts commit 89f344efe596cf79354b1d8749fe9f801ad595d8.
    
    Reason for revert: Revert the revert with a fix
    
    Bug: 472307879
    Test: atest
    Flag: com.android.healthfitness.flags.device_udi
    Change-Id: Ia68c726ca92fe8ab306e39c59d5c6930f4198ccc

--- apk/src/com/android/healthconnect/controller/shared/HealthPermissionReader.kt
+++ apk/src/com/android/healthconnect/controller/shared/HealthPermissionReader.kt
@@ -526,6 +526,7 @@ constructor(
                 !AconfigFlagHelper.isActivityIntensityEnabled()
             HealthPermissions.READ_NICOTINE_INTAKE,
             HealthPermissions.WRITE_NICOTINE_INTAKE -> !AconfigFlagHelper.isNicotineIntakeEnabled()
+            HealthPermissions.WRITE_DEVICE_UDI -> true // We do not show this permission in UI
             else -> !isPermissionEnabled(permission)
         }
     }
